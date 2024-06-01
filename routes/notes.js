648660ad750f4d61381245e27767ec6aebe278fd
PyroLabyrinth <2101115031yo@cyber-u.ac.jp>
2024-06-02 08:24:42 +0900

    connect mongodb

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://cu2101115031:Hikaru0712@cluster0.3rpzwez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+//データベースコレクションを設定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+//idが１のドキュメント取得
+const query = {id:1};
+const note = await notes.findOne(query);
+
+res.json(note.title);
+})
+/**
 //レスポンスのデータ（ノート全体）
 const responseObjectDataAll = {
     textObject:{
@@ -11,6 +28,7 @@ const responseObjectDataAll = {
 
     },
 };
+*/
 
 /**
  * メモを全件取得するAPI
@@ -20,9 +38,12 @@ const responseObjectDataAll = {
  * @returns{string} data.text - 内容
  */
 
+/**
 router.get('/',function(req,res,next){
     //全件取得して返す
     res.json(responseObjectDataAll);
 })
+*/
+
 
 module.exports = router;
